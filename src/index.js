2322f08c3714c5b70d3eb89ba597f80b7943a6a6
Julien Gonzalez <machette@hotmail.fr>
2018-01-12 12:38:30 +0100

    Use export.module to expose scrollback

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-import ScrollBack from './js/ScrollBack';
+import ScrollBack from './js/ScrollBack'
 
-
-var header = document.querySelector("header");
-
-const headerScrollBack = new ScrollBack(header, {
-    tolerance: {
-        up: 5,
-        down: 5
-    },
-    offset: 80,
-    scroller: window,
-    classes: {
-        initial: 'header',
-        pinned: 'header-pinned',
-        unpinned: 'header-unpinned',
-        scrolled: 'header-scrolled'
-    }
-});
+module.exports = ScrollBack
